fb08a8c180a0fe063cc8b2464b2d8a9e6cb2b45c
codystack <ebuzzadvert@gmail.com>
2022-10-31 13:40:24 +0100

    new edit

--- src/pages/About.js
+++ src/pages/About.js
@@ -17,7 +17,7 @@ export default function About() {
             <div className="container dark-mode position-relative zindex-2 py-5 mb-4 mb-sm-5">
                 <div className="row pb-5 mb-2 mb-sm-0 mb-lg-3">
                     <div className="col-lg-8 col-xl-8 text-center pt-5 mx-auto">
-                        <h1 className="display-2 pb-2 pb-sm-3 text-black" style={{fontFamily: 'pt-serif, serif', fontWeight: '700', fontStyle: 'normal'}}>We’re on a mission to make it easy to live a healthy life.</h1>
+                        <h1 className="display-2 pb-2 pb-sm-3 text-black" style={{fontFamily: 'pt-serif, serif', fontWeight: '700', fontStyle: 'normal'}}>About us</h1>
                     </div>
                 </div>
             </div>
@@ -34,15 +34,16 @@ export default function About() {
                     <div className="fs-sm text-uppercase mb-3">Who we are</div>
                     <h2 className="display-6">Healthy food for busy people.</h2>
                 </div>
-                <div className="col-md-7 col-xl-7 pt-1 pt-sm-2 pt-md-5">
-                    <p className="fs-lg">EasyFit was established on the principle that real food should be convenient and delicious, and we live for making healthy meals that people are excited to eat. Our food contains zero preservatives, gluten, antibiotics, artificial sweeteners, hormones, or other junk.</p>
-                    <p className="fs-lg">We take gluten-free food (and cooking) seriously—and your trust is our #1. Because gluten-free meals are only as good as the kitchens they’re prepared in, we make sure our space is guaranteed safe.</p>
-                    <p className="fs-lg mb-0">We deliver fresh, never frozen meals directly to our customers doorstep. The fully-prepared-meals arrive ready to heat and eat in minutes. We want to change the way our customers think about pre-prepared meals, from how it’s cooked (by real chefs!) to how it gets delivered.</p>
+                <div className="col-md-7 col-xl-7 pt-1 pt-sm-2 pt-md-5 pb-5">
+                    <p className="fs-lg">Arinola Odunuga, a certified fitness trainer and nutrition coach first started Easyfit in 2021 to fill a missing element in the health and fitness industry in Nigeria - delicious, convenient and affordable meal prep. Taking the hassle out of planning and preparing healthy meals so you can stay on track with your nutrition goals.</p>
+                    <p className="fs-lg">We pride ourselves on using the freshest ingredients to create exciting meals based on science backed nutrition our clients can look forward to eating. Our foods contain low to medium glycemic load, no preservatives, no added sugars and zero trans fats. </p>
+                    <p className="fs-lg">We  don’t just save you the trip to the market/grocery store, we make your health and fitness journey a convenient, delicious and positive experience. No shopping. No cooking. No cleaning. No calculating calories and macros.</p>
+                    <p className="fs-lg">We deliver fresh, never frozen meals directly to your doorstep. Our fully-prepared-meals arrive ready to heat and eat in minutes.</p>
                 </div>
             </div>
         </section>
 
-        <section className="container py-5 my-1 my-sm-2 my-xl-4 my-xxl-5" data-aos="fade-up" data-aos-duration={500} data-aos-offset={250} data-disable-parallax-down="md">
+        {/* <section className="container py-5 my-1 my-sm-2 my-xl-4 my-xxl-5" data-aos="fade-up" data-aos-duration={500} data-aos-offset={250} data-disable-parallax-down="md">
             <div className="row align-items-center pt-2 pt-sm-3 pt-md-4 pt-lg-5 mt-xl-2 mt-xxl-3">
             <div className="col-md-6 col-xl-5 pb-3 pb-md-0 mb-4 mb-md-0">
                 <div className="ratio ratio-1x1 d-flex align-items-center position-relative rounded-circle overflow-hidden bg-size-cover mx-auto" style={{maxWidth: '530px', backgroundImage: 'url(assets/img/Arinola.jpeg)'}}>
@@ -61,7 +62,7 @@ export default function About() {
                 </div>
             </div>
             </div>
-        </section>
+        </section> */}
 
         {/* <section className="bg-dark position-relative overflow-hidden py-5">
             <div className="d-none d-dark-mode-block position-absolute top-0 start-0 w-100 h-100" style={{backgroundColor: 'rgba(255,255,255, .02)'}} />
